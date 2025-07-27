ec1cc6e593ca878690b07c7d8516285ba2f72416
Rizky Ananda <rizkyananda954@gmail.com>
2025-07-27 10:35:53 +0700

    update koneksi firebase dan deploy

--- config_firebase.py
+++ config_firebase.py
@@ -1,8 +1,10 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
+@st.cache_resource
 def init_firebase():
-    if not firebase_admin._apps:  # ✅ Cek apakah sudah inisialisasi sebelumnya
-        cred = credentials.Certificate("silastik.json")
+    if not firebase_admin._apps:
+        firebase_cred = json.loads(st.secrets["FIREBASE_JSON"])
+        cred = credentials.Certificate(firebase_cred)
         firebase_admin.initialize_app(cred)
     return firestore.client()
